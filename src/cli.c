b982cacc0199d9ce1a4eadd8b885e7e453afcb7f
Frank W Miller <frank@frankwmiller.net>
2021-11-02 19:52:10 +0000

    Broke out cli() from kmain.c to cli.c

---
+++ src/cli.c
@@ -0,0 +1,53 @@
+#include <ctype.h>
+#include <stdint.h>
+#include <string.h>
+
+#define PROMPT	"> "
+
+void kprintf(const char *fmt, ...);
+int uart_getchar();
+
+static int
+get_cmdline(char *cmdline, int len)
+{
+	int pos;
+	char ch;
+
+	memset(cmdline, 0, len);
+	for (pos = 0;;) {
+		ch = uart_getchar();
+
+		if (!isprint(ch) && ch != '\r' && ch != '\n' && ch != '\b')
+			continue;
+
+		if (ch == '\n' || ch == '\r') {
+			kprintf("\n");
+			break;
+		}
+		if (ch == '\b' && pos > 0) {
+			cmdline[--pos] = '\0';
+			kprintf("\b \b");
+
+		} else if (ch != '\b' && ch != '\r') {
+			cmdline[pos++] = ch;
+			kprintf("%c", ch);
+			if (pos == len - 1)
+				break;
+		}
+	}
+	return pos;
+}
+
+
+void cli()
+{
+	char cmdline[80];
+
+	kprintf(PROMPT);
+	for (;;) {
+		get_cmdline(cmdline, 80);
+		if (strlen(cmdline) > 0)
+			kprintf("%s\r\n", cmdline);
+		kprintf(PROMPT);
+	}
+}
--- src/kmain.c
+++ src/kmain.c
@@ -1,13 +1,9 @@
-#include <ctype.h>
 #include <stdint.h>
-#include <stdio.h>
-#include <string.h>
-
-#define PROMPT	"> "
 
 void kprintf(const char *fmt, ...);
 void ata_init();
-int uart_getchar();
+
+void cli();
 
 static void
 word_widths()
@@ -19,54 +15,13 @@ word_widths()
 	kprintf(" void * = %u (bytes)\r\n", sizeof(void *));
 }
 
-static int
-get_cmdline(char *cmdline, int len)
-{
-	int pos;
-	char ch;
-
-	memset(cmdline, 0, len);
-	for (pos = 0;;) {
-		ch = uart_getchar();
-
-		if (!isprint(ch) && ch != '\r' && ch != '\n' && ch != '\b')
-			continue;
-
-		if (ch == '\n' || ch == '\r') {
-			kprintf("\n");
-			break;
-		}
-		if (ch == '\b' && pos > 0) {
-			cmdline[--pos] = '\0';
-			kprintf("\b \b");
-
-		} else if (ch != '\b' && ch != '\r') {
-			cmdline[pos++] = ch;
-			kprintf("%c", ch);
-			if (pos == len - 1)
-				break;
-		}
-	}
-	return pos;
-}
-
-
 void kmain()
 {
-	char cmdline[80];
-
-	kprintf("\nRoadrunner 64-bit\n");
+	kprintf("\r\nRoadrunner 64-bit\r\n");
 
 	word_widths();
 	ata_init();
 
-	kprintf("Type ctrl-a x to exit\n");
-
-	kprintf(PROMPT);
-	for (;;) {
-		get_cmdline(cmdline, 80);
-		if (strlen(cmdline) > 0)
-			kprintf("%s\r\n", cmdline);
-		kprintf(PROMPT);
-	}
+	kprintf("Type ctrl-a x to exit\r\n");
+	cli();
 }
