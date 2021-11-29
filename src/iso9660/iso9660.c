7e2b77f2e95406413a5d4bd91af5d5fbd799dbd9
Frank W Miller <frank@frankwmiller.net>
2021-11-29 18:55:25 +0000

    Reading directory records

--- src/iso9660/iso9660.c
+++ src/iso9660/iso9660.c
@@ -1,9 +1,6 @@
-#include <ctype.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <string.h>
 #include <sys/ata.h>
-#include <sys/ioctl.h>
 #include <sys/iso9660.h>
 #include <sys/sys.h>
 
@@ -11,16 +8,18 @@ typedef uint32_t lba_t;
 
 static uint8_t pri_vol_desc[ATAPI_SECTOR_SIZE];
 static uint8_t path_table[ATAPI_SECTOR_SIZE];
+static uint8_t root_dir[ATAPI_SECTOR_SIZE];
 
 /*
  * buf must be at least ATAPI_SECTOR_SIZE = 2048 bytes and is assumed to
  * be zeroed
  */
-static int
-iso9660_read_blk(atap_t part, lba_t lba, uint8_t * buf)
+static int iso9660_read_blk(atap_t part, lba_t lba, uint8_t * buf)
 {
 	struct seek seekargs;
 	int result;
+
+	kprintf("blkno %u\r\n", lba);
 
 	/* Convert lba to sector */
 	uint32_t sectorno = lba << 2;
@@ -36,8 +35,7 @@ iso9660_read_blk(atap_t part, lba_t lba, uint8_t * buf)
 	return ata_read(part, buf, ATAPI_SECTOR_SIZE);
 }
 
-static int
-iso9660_verify_primary_volume(volume_descriptor_t vol)
+static int iso9660_verify_primary_volume(volume_descriptor_t vol)
 {
 	if (vol->type != VOLUME_DESCRIPTOR_TYPE_PRIMARY ||
 	    vol->id[0] != 'C' || vol->id[1] != 'D' || vol->id[2] != '0' ||
@@ -46,92 +44,8 @@ iso9660_verify_primary_volume(volume_descriptor_t vol)
 	return 0;
 }
 
-static void
-iso9660_dump_primary_volume(primary_volume_descriptor_t pri)
+void iso9660_init()
 {
-	int i;
-
-	kprintf("ISO9660 primary volume\r\n");
-
-	kprintf("system identifier [");
-	for (i = 0; i < 32; i++)
-		kprintf("%c", pri->sys_id[i]);
-	kprintf("]\r\n");
-
-	kprintf("volume identifier [");
-	for (i = 0; i < 32; i++)
-		kprintf("%c", pri->vol_id[i]);
-	kprintf("]\r\n");
-
-	kprintf("%u blks (%u MB)\r\n",
-		pri->vol_space_size_le,
-		pri->vol_space_size_le * ATAPI_SECTOR_SIZE / 0x100000);
-
-	kprintf("logical blk size %u bytes\r\n", pri->logical_blk_size_le);
-	kprintf("path table size %u bytes\r\n", pri->path_table_size_le);
-	kprintf("path table location blk %u\r\n", pri->path_table_loc_le);
-}
-
-static void
-iso9660_dump_path_table_record(path_table_record_t rec)
-{
-	kprintf("%10u ", rec->dir_id_len);
-	kprintf("%15u ", rec->ext_att_rec_len);
-	kprintf("%12u ", rec->lba);
-	kprintf("%12u ", rec->parent_dirno);
-
-	for (int i = 0; i < rec->dir_id_len; i++) {
-		char *str = ((char *)rec) + sizeof(struct path_table_record);
-		kprintf("%c", *(str + i));
-	}
-	kprintf("\r\n");
-}
-
-static void
-iso9660_dump_path_table(primary_volume_descriptor_t pri, uint8_t * buf)
-{
-	kprintf("dir_id_len ext_att_rec_len lba          ");
-	kprintf("parent_dirno path\r\n");
-
-	for (int pos = 0; pos < pri->path_table_size_le;) {
-		path_table_record_t rec = (path_table_record_t) (buf + pos);
-		iso9660_dump_path_table_record(rec);
-
-		unsigned reclen = sizeof(struct path_table_record) +
-		    rec->dir_id_len;
-		if (rec->dir_id_len % 2 == 1)
-			reclen++;
-		pos += reclen;
-	}
-}
-
-void
-iso9660_dump_directory(uint8_t * buf)
-{
-	directory_record_t rec = (directory_record_t) buf;
-
-	kprintf("dir_rec_len %u\r\n", rec->dir_rec_len);
-	kprintf("ext_att_rec_len %u\r\n", rec->ext_att_rec_len);
-	kprintf("lba %u\r\n", rec->lba_le);
-	kprintf("size %u\r\n", rec->size_le);
-	kprintf("flags 0x%02x\r\n", rec->flags);
-	kprintf("unit_size %u\r\n", rec->unit_size);
-	kprintf("interleave_gap_size %u\r\n", rec->interleave_gap_size);
-	kprintf("vol_seqno %u\r\n", rec->vol_seqno_le);
-	kprintf("file_id_len %u\r\n", rec->file_id_len);
-
-	kprintf("[");
-	for (int i = 0; i < rec->file_id_len; i++) {
-		char *str = (char *)(buf + sizeof(struct directory_record));
-		kprintf("%c", *(str + i));
-	}
-	kprintf("]\r\n");
-}
-
-void
-iso9660_init()
-{
-	uint8_t buf[ATAPI_SECTOR_SIZE];
 	atap_t atap = ata_get_primary_partition();
 	primary_volume_descriptor_t pri;
 	int result;
@@ -156,5 +70,14 @@ iso9660_init()
 	iso9660_read_blk(atap, pri->path_table_loc_le, path_table);
 	iso9660_dump_path_table(pri, path_table);
 
-	iso9660_dump_directory((uint8_t *) pri->root_dir_entry);
+	iso9660_dump_directory((uint8_t *) pri->root_dir_entry,
+				sizeof(pri->root_dir_entry));
+
+	/* Read root directory */
+	directory_record_t rec = (directory_record_t) pri->root_dir_entry;
+	memset(root_dir, 0, ATAPI_SECTOR_SIZE);
+	iso9660_read_blk(atap, rec->lba_le, root_dir);
+
+	bufdump(root_dir, ATAPI_SECTOR_SIZE);
+	iso9660_dump_directory((uint8_t *) root_dir, ATAPI_SECTOR_SIZE);
 }
---
+++ src/iso9660/iso9660_dump.c
@@ -0,0 +1,95 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <sys/ata.h>
+#include <sys/iso9660.h>
+
+void iso9660_dump_primary_volume(primary_volume_descriptor_t pri)
+{
+	int i;
+
+	kprintf("ISO9660 primary volume\r\n");
+
+	kprintf("system identifier [");
+	for (i = 0; i < 32; i++)
+		kprintf("%c", pri->sys_id[i]);
+	kprintf("]\r\n");
+
+	kprintf("volume identifier [");
+	for (i = 0; i < 32; i++)
+		kprintf("%c", pri->vol_id[i]);
+	kprintf("]\r\n");
+
+	kprintf("%u blks (%u MB)\r\n",
+		pri->vol_space_size_le,
+		pri->vol_space_size_le * ATAPI_SECTOR_SIZE / 0x100000);
+
+	kprintf("logical blk size %u bytes\r\n", pri->logical_blk_size_le);
+	kprintf("path table size %u bytes\r\n", pri->path_table_size_le);
+	kprintf("path table location blk %u\r\n", pri->path_table_loc_le);
+}
+
+static void iso9660_dump_path_table_record(path_table_record_t rec)
+{
+	kprintf("%10u ", rec->dir_id_len);
+	kprintf("%15u ", rec->ext_att_rec_len);
+	kprintf("%12u ", rec->lba);
+	kprintf("%12u ", rec->parent_dirno);
+
+	for (int i = 0; i < rec->dir_id_len; i++) {
+		char *str = ((char *)rec) + sizeof(struct path_table_record);
+		kprintf("%c", *(str + i));
+	}
+	kprintf("\r\n");
+}
+
+void iso9660_dump_path_table(primary_volume_descriptor_t pri, uint8_t * buf)
+{
+	kprintf("dir_id_len ext_att_rec_len lba          ");
+	kprintf("parent_dirno path\r\n");
+
+	for (int pos = 0; pos < pri->path_table_size_le;) {
+		path_table_record_t rec = (path_table_record_t) (buf + pos);
+		iso9660_dump_path_table_record(rec);
+
+		unsigned reclen = sizeof(struct path_table_record) +
+		    rec->dir_id_len;
+		if (rec->dir_id_len % 2 == 1)
+			reclen++;
+		pos += reclen;
+	}
+}
+
+void iso9660_dump_directory(uint8_t * buf, int size)
+{
+	directory_record_t rec;
+
+	for (int pos = 0; pos < size;) {
+		rec = (directory_record_t) (buf + pos);
+		if (rec->dir_rec_len == 0)
+			break;
+
+		kprintf("\r\n[");
+		for (int i = 0; i < rec->file_id_len; i++) {
+			char *str =
+			    (char *)(buf + sizeof(struct directory_record));
+			char ch = *(str + i);
+			if (isupper(ch) || isdigit(ch) || ch == '_')
+				kprintf("<%c>", ch);
+			else
+				kprintf("(%02x)", ch);
+		}
+		kprintf("]\r\n");
+		kprintf("dir_rec_len         %u\r\n", rec->dir_rec_len);
+		kprintf("ext_att_rec_len     %u\r\n", rec->ext_att_rec_len);
+		kprintf("lba                 %u\r\n", rec->lba_le);
+		kprintf("size                %u\r\n", rec->size_le);
+		kprintf("flags               0x%02x\r\n", rec->flags);
+		kprintf("unit_size           %u\r\n", rec->unit_size);
+		kprintf("interleave_gap_size %u\r\n",
+			rec->interleave_gap_size);
+		kprintf("vol_seqno           %u\r\n", rec->vol_seqno_le);
+		kprintf("file_id_len         %u\r\n", rec->file_id_len);
+
+		pos += rec->dir_rec_len;
+	}
+}
