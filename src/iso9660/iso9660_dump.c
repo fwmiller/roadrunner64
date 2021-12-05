2d29f6093c565cb2f618c770143bbc978192bc35
Frank W Miller <frank@frankwmiller.net>
2021-12-05 17:50:02 +0000

    Updates to iso9660_dump()

--- src/iso9660/iso9660_dump.c
+++ src/iso9660/iso9660_dump.c
@@ -59,6 +59,76 @@ void iso9660_dump_path_table(primary_volume_descriptor_t pri, uint8_t * buf)
 	}
 }
 
+void
+iso9660_dump_file_id(directory_record_t rec)
+{
+	char *s = ((char *) rec) + sizeof(struct directory_record);
+
+	for (int i = 0; i < rec->file_id_len; i++) {
+		char ch = *(s + i);
+		if (isprint(ch))
+			kprintf("%c", ch);
+		else
+			kprintf("(%02x)", ch);
+	}
+}
+
+void
+iso9660_dump_record_date(directory_record_t rec)
+{
+	uint8_t *rec_date = (uint8_t *) rec->record_date;
+	kprintf("%02u:", rec_date[3]);
+	kprintf("%02u:", rec_date[4]);
+	kprintf("%02u ", rec_date[5]);
+	kprintf("%02u-", rec_date[1]);
+	kprintf("%02u-", rec_date[2]);
+	kprintf("%04u ", rec_date[0] + 1900);
+}
+
+void
+iso9660_dump_flags(directory_record_t rec)
+{
+	uint8_t flags = rec->flags;
+
+	if (flags & FILE_FLAGS_EXISTENCE) {
+		kprintf("EXISTS");
+		flags &= ~FILE_FLAGS_EXISTENCE;
+		if (flags == 0)
+			return;
+		kprintf(" ");
+	}
+	if (flags & FILE_FLAGS_DIRECTORY) {
+		kprintf("DIRECTORY");
+		flags &= ~FILE_FLAGS_DIRECTORY;
+		if (flags == 0)
+			return;
+		kprintf(" ");
+	}
+	if (flags & FILE_FLAGS_ASSOCIATED_FILE) {
+		kprintf("ASSOCIATED-FILE");
+		flags &= ~FILE_FLAGS_ASSOCIATED_FILE;
+		if (flags == 0)
+			return;
+		kprintf(" ");
+	}
+	if (flags & FILE_FLAGS_RECORD) {
+		kprintf("RECORD");
+		flags &= ~FILE_FLAGS_RECORD;
+		if (flags == 0)
+			return;
+		kprintf(" ");
+	}
+	if (flags & FILE_FLAGS_PROTECTION) {
+		kprintf("PROTECTION");
+		flags &= ~FILE_FLAGS_PROTECTION;
+		if (flags == 0)
+			return;
+		kprintf(" ");
+	}
+	if (flags & FILE_FLAGS_MULTI_EXTENT)
+		kprintf("MULTI-EXTENT");
+}
+
 void iso9660_dump_directory(uint8_t * buf, int size)
 {
 	directory_record_t rec;
@@ -69,26 +139,30 @@ void iso9660_dump_directory(uint8_t * buf, int size)
 			break;
 
 		kprintf("\r\n[");
-		char *str = ((char *) rec) +
-			sizeof(struct directory_record);
-		for (int i = 0; i < rec->file_id_len; i++) {
-			char ch = *(str + i);
-			if (isprint(ch))
-				kprintf("%c", ch);
-			else
-				kprintf("(%02x)", ch);
-		}
+		iso9660_dump_file_id(rec);
 		kprintf("]\r\n");
-		kprintf("dir_rec_len         %u\r\n", rec->dir_rec_len);
-		kprintf("ext_att_rec_len     %u\r\n", rec->ext_att_rec_len);
-		kprintf("lba                 %u\r\n", rec->lba_le);
-		kprintf("size                %u\r\n", rec->size_le);
-		kprintf("flags               0x%02x\r\n", rec->flags);
-		kprintf("unit_size           %u\r\n", rec->unit_size);
-		kprintf("interleave_gap_size %u\r\n",
-			rec->interleave_gap_size);
-		kprintf("vol_seqno           %u\r\n", rec->vol_seqno_le);
-		kprintf("file_id_len         %u\r\n", rec->file_id_len);
+
+		kprintf("created ");
+		iso9660_dump_record_date(rec);
+		kprintf("\r\n");
+
+		kprintf("dir_rec_len %u ", rec->dir_rec_len);
+		kprintf("ext_att_rec_len %u ", rec->ext_att_rec_len);
+		kprintf("file_id_len %u\r\n", rec->file_id_len);
+		kprintf("lba %u ", rec->lba_le);
+		kprintf("size %u ", rec->size_le);
+
+		kprintf("flags 0x%02x ", rec->flags);
+		if (rec->flags > 0) {
+			kprintf("(");
+			iso9660_dump_flags(rec);
+			kprintf(")");
+		}
+		kprintf("\r\n");
+
+		kprintf("unit_size %u ", rec->unit_size);
+		kprintf("interleave_gap_size %u ", rec->interleave_gap_size);
+		kprintf("vol_seqno %u\r\n", rec->vol_seqno_le);
 
 		pos += rec->dir_rec_len;
 	}
