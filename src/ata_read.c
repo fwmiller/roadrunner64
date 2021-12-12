1a02502458e2597c620c17fa34826c043caa93a5
Frank W Miller <frank@frankwmiller.net>
2021-12-12 17:24:44 +0000

    Collapsed some source files together

--- src/ata_read.c
+++ src/ata_read.c
@@ -42,3 +42,42 @@ ata_read(void *dev, uint8_t * b, int len)
 	}
 	return 0;
 }
+
+static int
+ata_read_mbr(atap_t atap, uint8_t * b, int *len)
+{
+	struct seek seekargs;
+	int result;
+
+	seekargs.offset = 0;
+	seekargs.whence = SEEK_SET;
+	result = ata_seek(atap->atad, &seekargs);
+	if (result < 0)
+		return result;
+
+	result = ata_read(atap, b, *len);
+	if (result < 0)
+		return result;
+
+	return 0;
+}
+
+int
+ata_read_parttab(atad_t atad)
+{
+	uint8_t b[SECTOR_SIZE];
+	struct ata_partition atap;
+	int len, result;
+
+	memset(b, 0, SECTOR_SIZE);
+	atap.atad = atad;
+	atap.sectors = 0;
+	atap.offset = 0;
+	len = SECTOR_SIZE;
+	result = ata_read_mbr(&atap, b, &len);
+	if (result < 0)
+		return result;
+
+	load_parttab(b, atad->parttab);
+	return 0;
+}
--- src/isofs_dump.c
+++ src/isofs_dump.c
@@ -82,6 +82,65 @@ isofs_dump_flags(directory_record_t rec)
 }
 
 void
+isofs_dump_primary_volume(primary_volume_descriptor_t pri)
+{
+	int i;
+
+	kprintf("ISO9660 primary volume\r\n");
+
+	kprintf("system identifier [");
+	for (i = 0; i < 32; i++)
+		kprintf("%c", pri->sys_id[i]);
+	kprintf("]\r\n");
+
+	kprintf("volume identifier [");
+	for (i = 0; i < 32; i++)
+		kprintf("%c", pri->vol_id[i]);
+	kprintf("]\r\n");
+
+	kprintf("%u blks (%u MB)\r\n",
+		pri->vol_space_size_le,
+		pri->vol_space_size_le * ATAPI_SECTOR_SIZE / 0x100000);
+
+	kprintf("logical blk size %u bytes\r\n", pri->logical_blk_size_le);
+	kprintf("path table size %u bytes\r\n", pri->path_table_size_le);
+	kprintf("path table location blk %u\r\n", pri->path_table_loc_le);
+}
+
+static void
+isofs_dump_path_table_record(path_table_record_t rec)
+{
+	kprintf("%10u ", rec->dir_id_len);
+	kprintf("%15u ", rec->ext_att_rec_len);
+	kprintf("%12u ", rec->lba);
+	kprintf("%12u ", rec->parent_dirno);
+
+	for (int i = 0; i < rec->dir_id_len; i++) {
+		char *str = ((char *)rec) + sizeof(struct path_table_record);
+		kprintf("%c", *(str + i));
+	}
+	kprintf("\r\n");
+}
+
+void
+isofs_dump_path_table(primary_volume_descriptor_t pri, uint8_t * buf)
+{
+	kprintf("dir_id_len ext_att_rec_len lba          ");
+	kprintf("parent_dirno path\r\n");
+
+	for (int pos = 0; pos < pri->path_table_size_le;) {
+		path_table_record_t rec = (path_table_record_t) (buf + pos);
+		isofs_dump_path_table_record(rec);
+
+		unsigned reclen = sizeof(struct path_table_record) +
+		    rec->dir_id_len;
+		if (rec->dir_id_len % 2 == 1)
+			reclen++;
+		pos += reclen;
+	}
+}
+
+void
 isofs_dump_directory(uint8_t * buf, int size)
 {
 	directory_record_t rec;
