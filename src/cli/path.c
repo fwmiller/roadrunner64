ea99e61f9d1bf4b2d784bdf203a1692c640e2a89
Frank W Miller <frank@frankwmiller.net>
2022-08-19 17:19:26 +0000

    Work on path evaluation

--- src/cli/path.c
+++ src/cli/path.c
@@ -16,12 +16,19 @@ path_eval(char *path) {
     int len = strlen(path);
     for (i = 0, j = 0; i < len; i++) {
         if (path[i] == '/') {
+            /* Skip over multiple '/' separators */
             if (sep)
                 continue;
             else {
                 tmp[j++] = path[i];
                 sep = 1;
             }
+
+        } else if (path[i] == '.') {
+            /* Collapse '.' and '..' path elements */
+            if (sep && i > 0 && path[i - 1] != '/')
+                sep = 0;
+
         } else {
             if (sep)
                 sep = 0;
@@ -30,6 +37,9 @@ path_eval(char *path) {
     }
     memset(path, 0, CMD_LINE_LEN);
     strcpy(path, tmp);
+
+    printf("path_eval: %s\r\n", path);
+
     return path;
 }
 
