4c33dfbafadb75500c59ccb50d6c2d8e00c03272
Frank W Miller <frank@frankwmiller.net>
2023-06-10 23:51:40 +0000

    Header file updates for lwip

--- include/stdint.h
+++ include/stdint.h
@@ -1,9 +1,16 @@
 #ifndef __STDINT_H
 #define __STDINT_H
 
+typedef char int8_t;
+typedef short int16_t;
+typedef int int32_t;
+typedef long int64_t;
+
 typedef unsigned char uint8_t;
 typedef unsigned short uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long uint64_t;
+
+typedef unsigned int *uintptr_t;
 
 #endif
